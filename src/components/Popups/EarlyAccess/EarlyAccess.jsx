3d6b4be9c7b76b4c7997a52d73c808f1b699edfd
CPielet <cpielet@yemot.com>
2025-10-19 13:07:15 +0300

    early access popup

--- src/components/Popups/EarlyAccess/EarlyAccess.jsx
+++ src/components/Popups/EarlyAccess/EarlyAccess.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 import './EarlyAccess.css';
 
 export const EarlyAccess = ({ isOpen, onClose }) => {
-    // if (!isOpen) return null;
+//  if (!isOpen) return null;
 
     return (
-        <div className="popup-overlay">
-            <div className="popup">
-                  <img src="/XmarkPopups.svg" alt=""  id="XmarkPopups" />
+       <div className="popup-overlay" onClick={onClose}> 
+            <div className="popup" onClick={e => e.stopPropagation()}> 
+                  <img src="/XmarkPopups.svg" alt="close"  id="XmarkPopups" onClick={onClose} style={{ cursor: 'pointer' }}  />
                 <div className="popup-icon">
                  <img src="/CheckmarkG.svg" alt=""  id="CheckmarkG" />
                 </div>
                 <div className="popup-content">
               
-                <h2 className='a'>You've successfully signed up for early access!</h2>
+                <p className='a'>You've successfully signed up for early access!</p>
                 <p className='b'>Until then, build your dream phone system with our current app.</p>
                   </div>
                 <button className="popup-button" onClick={onClose}>
