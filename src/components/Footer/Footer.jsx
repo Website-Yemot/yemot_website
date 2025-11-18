50a524a37316c0e0b654a4bcad760fa3509cf108
T Klein <tkleinkaufman@yemot.com>
2025-11-18 14:11:16 +0200

    footer responcive

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,43 +1,66 @@
+import React from 'react';
 import "./Footer.css";
-  const scrollToSection = (id) => {
+// הפונקציה לגלילה (scroll) נשארה זהה
+const scrollToSection = (id) => {
   const element = document.getElementById(id);
   if (element) {
     element.scrollIntoView({ behavior: "smooth" });
   }
 };
+
 export const Footer = () => {
+  // סגנון ה-CSS מוכנס ישירות לרכיב ה-React באמצעות תגית <style>
+
+
   return (
-    <div className="footer">
-      <div className="frame-2">
-        <div className="div-2">
-       
-  <div className="logo-block">
-    <img className="img" alt="vector" src={'yemotlogo.png'} />
-    <div className="text-wrapper-3">
-      Yemot. Build any phone system<br />you could just dream of.
-    </div>
-  </div>
+    <>
+      <div className="app-footer">
+        
+        {/* FOOTER CONTENT */}
+        <div className="footer-content-wrapper">
+          
+          {/* עמודת לוגו ותיאור */}
+          <div className="footer-logo-block">
+            <img className="footer-logo-img" alt="vector" src={'yemotlogo.png'} />
+            <div className="footer-description">
+              Yemot. Build any phone system you could just dream of.
+            </div>
+          </div>
+          
+          {/* עמודת מוצר */}
+          <div className="footer-column">
+            <div className="footer-column-title">Product</div>
+            <div className="footer-link" onClick={() => scrollToSection("demos")}>
+              Phone Systems
+            </div>
+            <div className="footer-link" onClick={() => scrollToSection("freefeatures")}>
+              Features
+            </div>
+          </div>
 
-        </div>
+          {/* עמודת פתרונות */}
+          <div className="footer-column">
+            <div className="footer-column-title">Solutions</div>
+            <div className="footer-link" onClick={() => scrollToSection("whyus")}>
+              Why us
+            </div>
+            <div className="footer-link" onClick={() => scrollToSection("pricing")}>
+              Pricing
+            </div>
+          </div>
 
-        <div className="div-2">
-          <div className="text-wrapper-2">Product</div>
-          <div className="text-wrapper-3" onClick={() => scrollToSection("demos")}>Phone Systems</div>
-          <div className="text-wrapper-3" onClick={() => scrollToSection("freefeatures")}>Features</div>
-        </div>
-
-        <div className="div-2">
-          <div className="text-wrapper-2">Solutions</div>
-          <div className="text-wrapper-3" onClick={() => scrollToSection("whyus")}>Why us</div>
-          <div className="text-wrapper-3"onClick={() => scrollToSection("pricing")}>Pricing</div>
-        </div>
-
-        <div className="div-2">
-          <div className="text-wrapper-2">Get in touch</div>
-          <div className="text-wrapper-3"onClick={() => scrollToSection("support")}>Contact</div>
-          <div className="text-wrapper-3" onClick={() => scrollToSection("support")}>Support</div>
+          {/* עמודת יצירת קשר */}
+          <div className="footer-column">
+            <div className="footer-column-title">Get in touch</div>
+            <div className="footer-link" onClick={() => scrollToSection("support")}>
+              Contact
+            </div>
+            <div className="footer-link" onClick={() => scrollToSection("support")}>
+              Support
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
